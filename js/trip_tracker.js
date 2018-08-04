3c86bb5588c14f1813dcee37736af213ea118fdc
StuartCharters <charters.stuart@gmail.com>
2018-08-04 11:22:10 +0100

    can delete specific entries in the table

--- js/trip_tracker.js
+++ js/trip_tracker.js
@@ -15,13 +15,29 @@ document.addEventListener('DOMContentLoaded', () => {
     const trip = new Trip(event.target);
     const tripList = document.querySelector(`#trip-details`);
     const tripDetails = document.createElement(`tr`)
+    tripDetails.classList.add(`trip-details`);
 
     for(let detail of Object.keys(trip)){
       const tripDetail = document.createElement(`td`);
       tripDetail.textContent = trip[detail];
       tripDetails.appendChild(tripDetail);
     };
+
+    const actionCell = document.createElement(`td`);
+    const deleteButton = document.createElement(`button`);
+    deleteButton.classList.add("button")
+    deleteButton.textContent = `Delete`;
+    actionCell.appendChild(deleteButton);
+    tripDetails.appendChild(actionCell);
+
+    deleteButton.addEventListener(`click`, () => {
+      tripList.removeChild(tripDetails);
+    });
+
     tripList.appendChild(tripDetails);
     detailForm.reset();
   });
+
+
+
 });
